22441ad77866d86d4020ad329f527edf224fcc4d
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-29 11:10:35 +0300

    starting views

--- personalgallery/views.py
+++ personalgallery/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Image, Location,Category
 
 # Create your views here.
+def index(request):
+    # #CREATE THE ATIVE LINKS FOR CATGORY
+    # category=request.GET.get('category')
+    # if category == None:
+    #     images = Image.objects.all()
+    # else:
+    #     images=Image.objects.filter(category__name=category)
+    #CREATE THE ACTIVE LINKS FOR LOCATION  
+    location=request.GET.get('location')
+    if location == None:
+        images=Image.objects.all()
+    else: 
+        images=Image.objects.filter(location__name=location)
+
+    categories=Category.objects.all()
+    location=Location.objects.all()
+    return render(request, 'personalgallery/index.html', {"images":images, "categories": categories, "location":location})
