e7bedd6cf30bbaa2cf54fc7739b7406d3f361292
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-29 13:11:37 +0300

    creating superuser

--- personalgallery/urls.py
+++ personalgallery/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 urlpatterns=[
     path('', views.index, name="indexpage"),
     path('search/', views.show_category, name="search_categories"),
-    # path('oneimage/<int:pk>', views.viewPhoto, name="viewPhoto"),
+    path('oneimage/<int:pk>', views.viewPhoto, name="viewPhoto"),
     # path('searchbylocation/', views.show_by_location, name="showlocation")
 ]
