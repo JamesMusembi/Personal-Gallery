e229f6a49e5d733463769d90dbe78cdbd4ca1f49
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-29 12:23:15 +0300

    updating timeZone

--- personalgallery/views.py
+++ personalgallery/views.py
@@ -1,3 +1,5 @@
+
+  
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import Image, Location,Category
@@ -20,3 +22,30 @@ def index(request):
     categories=Category.objects.all()
     location=Location.objects.all()
     return render(request, 'personalgallery/index.html', {"images":images, "categories": categories, "location":location})
+
+def viewPhoto(request, pk):
+    image=Image.objects.get(id=pk)  
+    return render(request, 'personalgallery/oneimage.html', {"image": image})
+
+def show_category(request):
+    if 'article' in request.GET and request.GET['article']:
+        category=request.GET.get("article")
+        searched_categories=Image.search_image(category)
+        message=f"{category}"
+
+        return render(request, 'personalgallery/search.html', {"message": message, "articles": searched_categories })
+    else:
+        message="You have not searched any category"
+        return render(request, 'personalgallery/search.html', {"message":message})
+
+def show_by_location(request):
+    if 'places' in request.GET and request.GET['places']:
+        location=request.GET.get('places')
+        searched_locations=Image.filter_by_location(location)
+        message = f"{location}"
+
+        return render(request, 'personalgallery/searchlocation.html', {"message": message, "places": searched_locations})
+
+    else:
+        message="You have not searched any location"
+        return render(request, 'personalgallery/searchlocation.html', {"message":message})
