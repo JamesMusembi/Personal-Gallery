b060fe2838e3931d994cad04da92de3ccb5bd298
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-29 12:12:07 +0300

    connecting postgresql

--- gallery/settings.py
+++ gallery/settings.py
@@ -80,6 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':  'gallery',
+        'USER': 'moringa',
+        'PASSWORD':
         
     }
 }
