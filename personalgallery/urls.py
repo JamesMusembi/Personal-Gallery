cbecc90bdb7f9448d6a53b76ae58ba32290bb6d4
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-29 13:12:02 +0300

    creating superuser

--- personalgallery/urls.py
+++ personalgallery/urls.py
@@ -8,5 +8,5 @@ urlpatterns=[
     path('', views.index, name="indexpage"),
     path('search/', views.show_category, name="search_categories"),
     path('oneimage/<int:pk>', views.viewPhoto, name="viewPhoto"),
-    # path('searchbylocation/', views.show_by_location, name="showlocation")
+    path('searchbylocation/', views.show_by_location, name="showlocation")
 ]
