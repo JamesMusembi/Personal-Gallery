3a36588731fc18de11f16e4803c823c100d95568
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-30 10:12:57 +0300

    styling indexpage

--- gallery/settings.py
+++ gallery/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
